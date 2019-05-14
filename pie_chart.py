f666e2da5a3835bc50b2aa57f7b7cb81b0c57822
Operel <oriperelman@gmail.com>
2019-05-14 15:32:12 +0300

    deactivate annotation

--- pie_chart.py
+++ pie_chart.py
@@ -3,6 +3,7 @@ import random
 import math
 import matplotlib.pyplot as plt
 
+# work in progress
 # TODO: multiple axes: plotting single pie, annotation, legends, layout and style
 
 plt.style.use('./Kqlmagic.mplstyle')
@@ -31,63 +32,63 @@ for i, tab in enumerate(tabs):
 #     print(p)
 
 # annotation template
-annots = []
-for ax in axes:
-    annot = ax.annotate(
-        '',
-        xy=(0, 0),
-        xytext=(0, 0),
-        textcoords='offset points',
-        arrowprops=dict(arrowstyle='->', connectionstyle='angle, angleA=90, angleB=-10, rad=5')
-    )
-
-    annot.set_visible(False)
-    annots.append(annot)
-print(annots)
-
-def update_annot(t, i, patch):
-    theta1, theta2 = patch.theta1, patch.theta2
-    center, r = patch.center, patch.r
-    x = r * np.cos(math.pi / 180 * theta1 + center[0])
-    y = np.sin(math.pi / 180 * theta1 + center[1])
-    annot.xy = (x, y)
-    if 0 < theta1 < 90:
-        annot.set_x(x + 10)
-        annot.set_y(y + 40)
-    elif 90 < theta1 < 180:
-        annot.set_x(x - 80)
-        annot.set_y(y + 40)
-    elif 180 < theta1 < 270:
-        annot.set_x(x - 80)
-        annot.set_y(y - 40)
-    else:
-        annot.set_x(x + 10)
-        annot.set_y(y - 40)
-    text = f'{list(tabs[t].keys())[i]}: {list(tabs[t].values())[i]}'
-    annot.set_text(text)
-    annot.set_bbox(
-                dict(
-                    boxstyle='round',
-                    facecolor=colors[i],
-                    lw=0,
-                    alpha=0.8
-                    )
-                )
-
-def hover(event):
-    vis = annot.get_visible()
-    if event.inaxes:
-        for t, pie in enumerate(plots):
-            for i, patch in enumerate(pie[0]):
-                cont, ind = patch.contains(event)
-                if cont:
-                    update_annot(t, i, patch)
-                    annot.set_visible(True)
-                    fig.canvas.draw_idle()
-                    return
-    if vis:
-        annot.set_visible(False)
-        fig.canvas.draw_idle()
+# annots = []
+# for ax in axes:
+#     annot = ax.annotate(
+#         '',
+#         xy=(0, 0),
+#         xytext=(0, 0),
+#         textcoords='offset points',
+#         arrowprops=dict(arrowstyle='->', connectionstyle='angle, angleA=90, angleB=-10, rad=5')
+#     )
+#
+#     annot.set_visible(False)
+#     annots.append(annot)
+# print(annots)
+#
+# def update_annot(t, i, patch):
+#     theta1, theta2 = patch.theta1, patch.theta2
+#     center, r = patch.center, patch.r
+#     x = r * np.cos(math.pi / 180 * theta1 + center[0])
+#     y = np.sin(math.pi / 180 * theta1 + center[1])
+#     annot.xy = (x, y)
+#     if 0 < theta1 < 90:
+#         annot.set_x(x + 10)
+#         annot.set_y(y + 40)
+#     elif 90 < theta1 < 180:
+#         annot.set_x(x - 80)
+#         annot.set_y(y + 40)
+#     elif 180 < theta1 < 270:
+#         annot.set_x(x - 80)
+#         annot.set_y(y - 40)
+#     else:
+#         annot.set_x(x + 10)
+#         annot.set_y(y - 40)
+#     text = f'{list(tabs[t].keys())[i]}: {list(tabs[t].values())[i]}'
+#     annot.set_text(text)
+#     annot.set_bbox(
+#                 dict(
+#                     boxstyle='round',
+#                     facecolor=colors[i],
+#                     lw=0,
+#                     alpha=0.8
+#                     )
+#                 )
+#
+# def hover(event):
+#     vis = annot.get_visible()
+#     if event.inaxes:
+#         for t, pie in enumerate(plots):
+#             for i, patch in enumerate(pie[0]):
+#                 cont, ind = patch.contains(event)
+#                 if cont:
+#                     update_annot(t, i, patch)
+#                     annot.set_visible(True)
+#                     fig.canvas.draw_idle()
+#                     return
+#     if vis:
+#         annot.set_visible(False)
+#         fig.canvas.draw_idle()
 
 # figure and axes chart_properties
 for ax in axes:
@@ -102,6 +103,6 @@ for ax in axes:
     )
     ax.set_aspect('equal')
 
-fig.canvas.mpl_connect('motion_notify_event', hover)
+# fig.canvas.mpl_connect('motion_notify_event', hover)
 
 plt.show()
